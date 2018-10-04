e73fd918381e37c5adfe8fa4d758a251effeb3f4
anand.ujjwal <anand.ujjwal@inmobi.com>
2018-10-04 11:25:45 +0530

    Security change

---
+++ src/main/java/com/inmobi/platform/elasticyarn/gateway/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.inmobi.platform.elasticyarn.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("password1").roles("USER", "ADMIN")
+                .and()
+                .withUser("user2").password("password2").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/babar").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/countfetch").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/submityarnjar").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/submitsparkjar").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/adduser").hasRole("ADMIN")
+                .antMatchers("/addgroup").hasRole("ADMIN")
+                .anyRequest().authenticated().and().csrf().disable();
+    }
+}
